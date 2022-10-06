cfa750b5837a072f4d894d19c01240284c0ca33a
Hiren Ghosh <hiren.ghosh@gmail.com>
2022-10-06 12:38:33 +0200

    Update shanky_plot_V1.1.R

--- shanky_plot_V1.1.R
+++ shanky_plot_V1.1.R
@@ -1,16 +1,14 @@
 ################################################################################
-## Project : Covid predition
+## Project : NDM-ENT: Shanky plot
 ## Date: 12th July, 2021
 ## Written by : Dr. Hiren Ghosh, University medical center Freiburg, Germany
 ## hiren.ghosh@gmail.com
 ## more information are bellow:
 ########################### load library #######################################
-
 library(highcharter)
 library(htmlwidgets)
 library(dplyr)
 set.seed(111)
-
 ##########################generate a test data set##############################
 #to <- sample(x=c)
 t0 <- sample(x=c("HospA","HospB","HospC"),size = 100, replace = TRUE)
@@ -20,7 +18,6 @@ t3 <- sample(x=c("NDM1","NDM2", "NDM3", "NDM4"),size = 10, replace = TRUE)
 d<- data.frame(cbind(t0,t1,t2,t3))
 names(d) <- c('Hospital','Bug','outcome', 'resitance')
 head(d)
-
 #########################plot a shanky plot#####################################
 hchart(data_to_sankey(d), "sankey", name = "Patient Outcomes")%>%
   hc_plotOptions(series = list(dataLabels = list( style = list(fontSize = "10px")))) 
