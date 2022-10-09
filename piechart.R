2c644b0f2efab83edcae7d497af326cf855eea59
hirenbioinfo <hiren.ghosh@gmail.com>
2022-10-09 13:00:02 +0200

    make_table

--- piechart.R
+++ piechart.R
@@ -1,22 +1,25 @@
 library(plotly)
 
-USPersonalExpenditure <- data.frame("Categorie" = rownames(USPersonalExpenditure), USPersonalExpenditure)
-data <- USPersonalExpenditure[, c('Categorie', 'X1960')]
-
-colors <- c('rgb(211,94,96)', 'rgb(128,133,133)', 'rgb(144,103,167)', 'rgb(171,104,87)', 'rgb(114,147,203)')
-
-fig <- plot_ly(data, labels = ~Categorie, values = ~X1960, type = 'pie',
-               textposition = 'inside',
-               textinfo = 'label+percent',
-               insidetextfont = list(color = '#FFFFFF'),
-               hoverinfo = 'text',
-               text = ~paste('$', X1960, ' billions'),
-               marker = list(colors = colors,
-                             line = list(color = '#FFFFFF', width = 1)),
-               #The 'pull' attribute can also be used to create space between the sectors
-               showlegend = FALSE)
-fig <- fig %>% layout(title = 'United States Personal Expenditures by Categories in 1960',
-                      xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-                      yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+fig <- plot_ly(
+  type = 'table',
+  header = list(
+    values = c('<b>EXPENSES</b>', '<b>Q1</b>','<b>Q2</b>','<b>Q3</b>','<b>Q4</b>'),
+    line = list(color = '#506784'),
+    fill = list(color = '#119DFF'),
+    align = c('left','center'),
+    font = list(color = 'white', size = 12)
+  ),
+  cells = list(
+    values = rbind(
+      c('Salaries', 'Office', 'Merchandise', 'Legal', '<b>TOTAL</b>'),
+      c(1200000, 20000, 80000, 2000, 1212000),
+      c(1300000, 20000, 70000, 2000, 1392000),
+      c(1300000, 20000, 120000, 2000, 1442000),
+      c(1400000, 20000, 90000, 2000, 1412000)),
+    line = list(color = '#506784'),
+    fill = list(color = c('#25FEFD', 'white')),
+    align = c('left', 'center'),
+    font = list(color = c('#506784'), size = 12)
+  ))
 
 fig
